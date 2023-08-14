8a84265b99c46846320a72a983172f9a4a13687a
tenpei999 <tenpei999@gmail.com>
2023-08-14 12:24:01 +0900

    ファイル分割

--- wp-content/plugins/myfirstPlugin/build/index.asset.php
+++ wp-content/plugins/myfirstPlugin/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '25a0e18a37f4e3498083');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'da841a36a4597e1efa18');

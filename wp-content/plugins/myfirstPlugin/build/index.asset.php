8182103b08935fdf2b17b32972f99ffd8eb44668
tenpei999 <tenpei999@gmail.com>
2023-09-09 00:44:04 +0900

    テストデプロイ#6

--- wp-content/plugins/myfirstPlugin/build/index.asset.php
+++ wp-content/plugins/myfirstPlugin/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '508cd0e3bfdf0e3a54dd');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'b459b6c69c0a9af073dc');
--- wp-content/plugins/myfirstPlugin/build/index.js
+++ wp-content/plugins/myfirstPlugin/build/index.js
@@ -781,7 +781,7 @@ __webpack_require__.r(__webpack_exports__);
 
 // import save from './save';
 
-console.log("自動デプロイ成功!!再再来年こそVやねん");
+console.log("自動デプロイ成功!!VVやねん");
 
 /**
  * Every block starts by registering a new block type definition.

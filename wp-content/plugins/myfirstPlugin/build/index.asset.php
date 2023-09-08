f29d730951a7fffd998af614455a75472b446c5a
tenpei999 <tenpei999@gmail.com>
2023-09-08 23:09:03 +0900

    修正#5

--- wp-content/plugins/myfirstPlugin/build/index.asset.php
+++ wp-content/plugins/myfirstPlugin/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '2333131d7b93451fb43d');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '508cd0e3bfdf0e3a54dd');

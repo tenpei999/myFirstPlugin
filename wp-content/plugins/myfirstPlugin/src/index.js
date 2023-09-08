229f5ba6f263b30683d752e04d71c6d6859e27b4
tenpei999 <tenpei999@gmail.com>
2023-09-08 21:53:31 +0900

    テストデプロイ

--- wp-content/plugins/myfirstPlugin/src/index.js
+++ wp-content/plugins/myfirstPlugin/src/index.js
@@ -24,7 +24,7 @@ import Edit from './edit';
 // import save from './save';
 import metadata from './block.json';
 
-console.log("自動デプロイ成功!!Vやねん")
+console.log("自動デプロイ成功!!今年こそVやねん")
 
 /**
  * Every block starts by registering a new block type definition.

5fedc4be7703e534b598fd790f312b492062c6d7
tenpei999 <tenpei999@gmail.com>
2023-09-10 03:41:43 +0900

    テスト#8

--- wp-content/plugins/myfirstPlugin/build/index.asset.php
+++ wp-content/plugins/myfirstPlugin/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'b459b6c69c0a9af073dc');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ea69fea891ebe51a1977');

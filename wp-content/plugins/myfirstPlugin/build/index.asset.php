c0c262f52fad36669c4fcaf614afda3d59288af9
tenpei999 <tenpei999@gmail.com>
2023-09-10 19:10:11 +0900

    #10

--- wp-content/plugins/myfirstPlugin/build/index.asset.php
+++ wp-content/plugins/myfirstPlugin/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '0a088e114d4c9fbee624');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'cc952f49c4fc36579c80');

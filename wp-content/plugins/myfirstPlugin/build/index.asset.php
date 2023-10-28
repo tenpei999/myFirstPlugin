2c75096275fd16b716eb9572b75498015ba5d7a9
tenpei999 <tenpei999@gmail.com>
2023-10-29 00:47:06 +0900

    merged

--- wp-content/plugins/myfirstPlugin/build/index.asset.php
+++ wp-content/plugins/myfirstPlugin/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '911edf2fe1f52f4368a4');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'af870f0bc4981e94fc80');
